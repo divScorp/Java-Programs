3f37a0559071e9a6591860aa869d49a8be5b3e3f
Divyanshu Sharma <divdev404@gmail.com>
2017-09-03 15:07:48 +0530

    Update CountChar.java

--- String/CountChar.java
+++ String/CountChar.java
@@ -1,5 +1,16 @@
 import java.util.Scanner;
-
+/*
+Q= Occurence of each letter without using inbuilt function.
+	i/p - java is easy
+	o/p -	j - 1
+		a - 3
+		v - 1
+		  - 2
+		i - 1
+		s - 2
+		e - 1
+		y - 1
+*/
 public class CountChar{
  public static void main(String... arg){
 	Scanner sc = new Scanner(System.in);
@@ -25,6 +36,6 @@ public class CountChar{
 	 else
 	  count=0;
 	}
-}
+   }
 }
 	  
