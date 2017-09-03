c93e38c7838e7dff552242635d8db6601c693aca
Divyanshu Sharma <divdev404@gmail.com>
2017-09-03 15:13:01 +0530

    Update hollowString.java

--- String/hollowString.java
+++ String/hollowString.java
@@ -3,7 +3,7 @@ package String;
 public class hollowString {
 
 	public static void main(String[] args) {
-		String st1 = "HURRICANE";
+		String st1 = "DIVYANSHU";
 		int len = st1.length();
 		for (int i = len/2+1; i >= 1; i--) {
 			for (int j = 1; j <= len; j++) {
